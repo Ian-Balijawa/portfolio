a234ea4cc10bb9c69c7f4f282480cd3ae496d2df
ianbalijawa <ianbalijawa16@gmail.com>
2024-01-27 00:19:05 +0300

    [ui:fix] updates

--- context/themeProvider.tsx
+++ context/themeProvider.tsx
@@ -41,7 +41,7 @@ export function Providers({ children, ...props }: Props) {
             'winter',
          ]}
          enableSystem={false}
-         defaultTheme="night"
+         defaultTheme="black"
          {...props}
       >
          {children}
